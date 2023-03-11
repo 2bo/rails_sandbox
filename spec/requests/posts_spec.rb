a8fd5857204bc4ccfbb1fd29286c0d30bdd2c602
2bo <developer.2bo@gmail.com>
2023-03-11 23:27:51 +0900

    fix spec

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe 'Posts' do
                                       },
                                       {
                                         id: first_post.id,
-                                        title: 'post',
+                                        title: 'first post',
                                         body: first_post.body,
                                         created_at: first_post.created_at.iso8601(3),
                                         updated_at: first_post.updated_at.iso8601(3)
