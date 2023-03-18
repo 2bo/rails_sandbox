3d1520ca9803e27681ed53fde886e2cffab27f1e
2bo <developer.2bo@gmail.com>
2023-03-18 15:07:02 +0900

    rubocop workflow test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,20 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe Post do
-  describe '#valid?' do
-    describe 'title' do
+RSpec.describe Post, type: :model do
+  describe '#valid? '  do
+        describe 'title' do
       subject(:post) { build(:post, title:) }
-
       context 'when title is present' do
-        let(:title) { 'title' }
-
+        let(:title) { "title" }
         it { is_expected.to be_valid }
       end
-
       context 'when title is null' do
         let(:title) { nil }
-
         it { is_expected.to be_invalid }
       end
     end
