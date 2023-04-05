3a85941486d1a69551da4d64bed62257793d5d18
2bo <developer.2bo@gmail.com>
2023-04-05 21:31:48 +0900

    feat/change_to_review_test1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,10 @@
 
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(id: :desc)
-    render json: @posts
+    @pots = Post.all.order(id: :desc)
+    Comment.create(content: 'content', post: @posts.first)
+    pp 'hoge'
+    @comments = Comment.all
+    render json: @pots
   end
 end
