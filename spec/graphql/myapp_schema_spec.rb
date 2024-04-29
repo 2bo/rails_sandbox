27b5e4ec60b69b4d5fdd7c8adf467f2ff2f848a2
2bo <developer.2bo@gmail.com>
2024-04-29 22:17:19 +0900

    refactor spec

--- spec/graphql/myapp_schema_spec.rb
+++ spec/graphql/myapp_schema_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe MyappSchema do
     it 'is up to date' do
       current_defn = described_class.to_definition
       printout_defn = Rails.root.join('app/graphql/myapp_schema.graphql').read
-      # NOTE: exec rake if failed
-      # $ bundle exec rake graphql:schema:idl
-      expect(current_defn).to eq(printout_defn), 'Update the printed schema with `bundle exec rake dump_schema`'
+      expect(current_defn).to eq(printout_defn), 'Update the printed schema with `bundle exec rake graphql:schema:idl`'
     end
   end
 end
