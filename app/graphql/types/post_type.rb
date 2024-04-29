f4e6c152726a5727f30279c4dae16148d0649291
2bo <developer.2bo@gmail.com>
2024-04-30 00:02:41 +0900

    change to test graphql schema workflow

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,7 +3,7 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: false
+    field :title, String, null: true
     field :body, String
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
   end
