cb0357c2972c85dc8e275d192115c5ee367e93be
2bo <developer.2bo@gmail.com>
2024-04-30 18:14:12 +0900

    change to test

--- app/graphql/types/post_type.rb
+++ app/graphql/types/post_type.rb
@@ -3,9 +3,9 @@
 module Types
   class PostType < Types::BaseObject
     field :id, ID, null: false
-    field :title, String, null: false
+    field :title, String, null: true
     field :body, String
+    field :published, Boolean, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
-    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
 end
